57f621b7a47f35380e6a7ea3a6d099f0b551777e
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-07-12 13:45:41 -0400

    add post route for user table

--- api/users.js
+++ api/users.js
@@ -10,8 +10,18 @@ router.get("/", async (req, res, next) => {
 
     allUsers
       ? res.status(200).json(allUsers) 
-      : res.status(404).send("Campus List Not Found"); 
+      : res.status(404).send("User List Not Found"); 
   } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/AddUser", async (req, res, next) => {
+  try {
+    const createUser = await users.create(req.body);
+    res.status(201).json(createUser);
+  } catch (error) {
+    console.error(error);
     next(error);
   }
 });
