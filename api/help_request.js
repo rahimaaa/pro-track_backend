01a62a23670dcf669849984b57c47c55faa27620
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-07-13 20:54:31 -0400

    Add get route for specific help_requests using student emails

--- api/help_request.js
+++ api/help_request.js
@@ -27,5 +27,28 @@ router.get("/all", async (req, res, next) => {
   }
 });
 
+
+
+//Route for getting a specific specific request with student email
+router.get('/:email', async (req, res, next) => {
+    // Retrieving a specific request by email
+    try {req.params.email;
+      console.log(req.params)
+      const oneRequest = await help_request.findOne({ where: { stud_email: req.params.email }});
+  
+      if (!help_request) {
+    // If the users is not found, send a response with status code 404 
+    //And the error message (Users not found)
+        return res.status(404).json({ error: 'Request not found' });
+      }
+    // Sending a response with the retrieved users
+      res.json(oneRequest);
+    } catch (error) {
+  // Pass any error to the error handling
+      next(error);
+    }
+  });
+
+
 //Exports the router object.
 module.exports = router;
