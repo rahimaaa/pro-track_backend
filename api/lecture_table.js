dd1c3518ab9ee45d75e1df243696b22236314d42
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-07-14 10:38:36 -0400

    Add get all route for lecture_table

--- api/lecture_table.js
+++ api/lecture_table.js
@@ -3,4 +3,30 @@ const router = express.Router();
 const { lecture_table } = require("../db/models");
 
 
+//Route handler for the GETAll for all requests
+router.get("/all", async (req, res, next) => {
+    try { // Retrieve all users from the database
+      const allLectures = await lecture_table.findAll();
+  
+      console.log("these are all the requests: " + allLectures)
+  
+   // If there are requests, send a response with status code 200 
+   //and the Array of Requests
+  
+     allLectures
+        ? res.status(200).json(allLectures)
+        //If there are no lectures, send a response with status code 404
+  //And error message (No Lectures have been Posted) 
+        : res.status(404).send("No Lectures have been Posted");
+    } catch (error) {
+  
+      console.log(error);
+  // Log any errors that occur
+    }
+  });
+
+
+
+
+
 module.exports = router;
