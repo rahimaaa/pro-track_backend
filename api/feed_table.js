903cb6f027d0bd030ea9ba9af8edff2cf39ce665
Bruno Ogbennaya <bruno.ogbennaya@gmail.com>
2023-07-16 02:06:00 -0400

    Add remove route for feed_table using feed_id

--- api/feed_table.js
+++ api/feed_table.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { feed_table } = require("../db/models");
 
 //Add post route to create new help_request
-/*
+
 router.post("/", async (req, res, next) => {
     try {
       //deconstructing the constructor into the different fields
@@ -25,6 +25,20 @@ router.post("/", async (req, res, next) => {
     }
   });
 
-  */
+  //route to delete a specific announcement with stud_email(student email)
+router.delete("/:id", async (req, res, next) => {
+    try {
+      const remove = req.params.id;
+  
+      // Delete the help_request with the provided email from the database
+      await feed_table.destroy({ where: { id: remove } });
+  
+      res.json({ message: "Request resolved and deleted successfully" });
+      //Send response message (Request resolved and deleted successfully)
+    } catch (error) {
+      //Handling any errors that occur
+      next(error);
+    }
+  });
 
 module.exports = router;
