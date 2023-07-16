2643a4b65d362cca523c0d3d35d4e8083fcd425f
rahimaaa <rakhimakhabib@gmail.com>
2023-07-16 16:59:34 -0400

    Add: table releationships, change made index.js file in models

--- db/models/index.js
+++ db/models/index.js
@@ -5,9 +5,12 @@ const assignmentStatus = require("./assignmentStatus")
 const resources_table = require("./resources_table")
 
 // Associations Go Here
-
-
-
+User.hasMany(help_request);
+User.hasMany(resources_table);
+//User.hasMany(lecture_table);
+//User.hasMany(feed_table);
+User.belongsToMany(group, {through: "user_group"})
+Group.belongsToMany(User, {through: "user_group"})
 
 module.exports = {
   User,
