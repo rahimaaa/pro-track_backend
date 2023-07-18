8761f4c86b820dd25b9df1397e74b5cc7f14fd35
rahimaaa <rakhimakhabib@gmail.com>
2023-07-18 10:29:55 -0400

    changed the file names to capital

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 
 // Already mounted on /api/
-router.use("/user", require("./user"));
-router.use("/help-request", require("./helpRequest"));
-router.use("/lecture", require("./lecture"));
+router.use("/user", require("./User"));
+router.use("/help-request", require("./HelpRequest"));
+router.use("/lecture", require("./Lecture"));
 router.use("/feed", require("./Feed"));
-router.use("/assignment-status", require("./assignmentStatus"));
-router.use("/assignment", require("./assignment"));
-router.use("/resource", require("./resource"));
+router.use("/assignment-status", require("./AssignmentStatus"));
+router.use("/assignment", require("./Assignment"));
+router.use("/resource", require("./Resource"));
 
 // 404 Handling
 router.use((req, res, next) => {
